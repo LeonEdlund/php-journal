a8b9727349bb503a26d6b4c10a885c0d8e6ccc58
Leon Edlund <Leone.edlund@gmail.com>
2024-10-12 18:24:46 +0200

    add rss icon

--- views/partials/footer.php
+++ views/partials/footer.php
@@ -1,7 +1,12 @@
 <footer class="page-footer">
   <hr>
-  <section>
-    <p>Journal</p>
-    <p>Made by: Leon Edlund</p>
+  <section class="grid">
+    <div>
+      <p>Journal</p>
+      <p>Made by: Leon Edlund</p>
+    </div>
+    <div>
+      <a href="rss.php"><img src="images/rss-icon.svg" width="25" alt="rss logo"> Subscribe to rss feed</a>
+    </div>
   </section>
 </footer>
