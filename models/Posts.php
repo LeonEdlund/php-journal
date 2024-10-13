7a434ea0acfd5918df3ce91f452612c2aec11e16
Leon Edlund <Leone.edlund@gmail.com>
2024-10-13 16:05:34 +0200

    move publishPost to Post class

--- models/Posts.php
+++ models/Posts.php
@@ -8,18 +8,6 @@ class Posts extends Database
     parent::__construct();
   }
 
-  public function publishPost($title, $postText)
-  {
-    if (strlen($title) > 0 && strlen($postText) > 0) {
-      $query = 'INSERT INTO posts(author_id, title, post_text) VALUES (?, ?, ?)';
-      $this->query($query, [$_SESSION['user']['id'], $title, $postText]);
-
-      return 'Post uploaded!';
-    } else {
-      return 'Fill in all fields!';
-    }
-  }
-
   public function getNumberOfPosts()
   {
     $query = "SELECT * FROM posts";
@@ -29,6 +17,12 @@ class Posts extends Database
   public function getAllPosts($limit = "")
   {
     $query = "SELECT posts.*, users.first_name, users.last_name FROM posts INNER JOIN users ON users.id = posts .author_id ORDER BY posts.id DESC $limit";
+    return $this->query($query)->fetchAll();
+  }
+
+  public function getAllPostsFromUser($userId)
+  {
+    $query = "SELECT * FROM posts WHERE author_id = $userId ORDER BY posts.id DESC";
     return $this->query($query)->fetchAll();
   }
 }
