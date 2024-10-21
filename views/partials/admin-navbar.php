2b7d308bafbbd2181b10772c4ecd01c2f76b5e22
Leon Edlund <Leone.edlund@gmail.com>
2024-10-21 13:40:01 +0200

    remove js and use a tagg instead for buttons

--- views/partials/admin-navbar.php
+++ views/partials/admin-navbar.php
@@ -4,12 +4,8 @@
           <li>Admin Dashboard</li>
         </ul>
         <ul>
-          <li>
-            <button onclick="window.location = 'index.php?route=posts'">View Page</button>
-          </li>
-          <li>
-            <button id="log-out-btn" onclick="window.location = 'index.php?route=logout'">Log Out</button>
-          </li>
+          <li><a href='index.php?route=posts' class="btn-green">View Page</a></li>
+          <li><a href="index.php?route=logout" class="btn-red">Log Out</a></li>
         </ul>
       </nav>
     </header>
