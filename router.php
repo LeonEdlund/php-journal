e535bd125621f53671f96dc9f6545ba3dce3b74b
Leon Edlund <Leone.edlund@gmail.com>
2024-10-21 13:40:29 +0200

    move logout from controllers to utils

--- router.php
+++ router.php
@@ -5,11 +5,11 @@ $routs = [
   '/' => 'controllers/posts.php',
   'posts' => 'controllers/posts.php',
   'post' => 'controllers/post.php',
-  'login' => 'controllers/login.php',
   'dashboard' => 'controllers/dashboard/dashboard.php',
   'my-posts' => 'controllers/dashboard/my-posts.php',
   'edit-post' => 'controllers/dashboard/edit-post.php',
-  'logout' => 'controllers/logout.php',
+  'login' => 'controllers/login.php',
+  'logout' => 'utils/logout.php',
 ];
 
 if (array_key_exists($shortUri, $routs)) {
