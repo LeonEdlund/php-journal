ab046d847df34b152e4518fa6deccc0b8026824f
Leon Edlund <Leone.edlund@gmail.com>
2024-10-27 12:55:02 +0100

    adding comments

--- models/Authenticator.php
+++ models/Authenticator.php
@@ -1,24 +1,25 @@
 <?php
 
-/**
- * Authenticator class
- * 
- * Handles user authentication based on users credentials in the database
- * and saves authenticated user in session.
- */
 class Authenticator extends Database
 {
+  /**
+   * Establish connection to database using PDO.
+   * 
+   * @return void
+   */
   public function __construct()
   {
     parent::__construct();
   }
 
   /**
-   * Authenticates the user based on credentials in users table
+   * Authenticates the user based on credentials in users table.
    * 
+   * @param string $username provided username from log-in-form.
+   * @param string $pwd provided password from log-in-form.
    * @return bool 
-   * returns true if the credentials match and saves user in $_SESSION['user']
-   * returns false if user input does not match
+   * returns true if the credentials match and saves user in $_SESSION['user'].
+   * returns false if user input does not match.
    */
   public function authenticate($username, $pwd)
   {
@@ -34,8 +35,9 @@ class Authenticator extends Database
   }
 
   /** 
-   * Saves the user in SESSION 
+   * Saves the user in SESSION as key 'user'.
    * 
+   * @param object $user a anonyms object with user info from database.   
    * @return void
    */
   private function saveUserSession($user)
